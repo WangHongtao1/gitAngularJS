a1931b166749ef05664a3b79ee108030822eea67
WangHongtao1 <975061007@qq.com>
2018-12-18 17:15:30 +0800

    first commit

---
+++ gitAngularJS/gitAngularJS/Controllers/filtersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gitAngularJS.Controllers
+{
+    public class filtersController : Controller
+    {
+        // GET: filters
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
